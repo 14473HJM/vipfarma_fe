c3b3c6e8dbfdda3b249f6c06bc012e63189db82c
DiegoEzequielSosa <diegoezequielsosa@gmail.com>
2022-11-03 00:00:21 -0300

    clientes en main

--- src/app/Customer/list-customer/list-customer.component.ts
+++ src/app/Customer/list-customer/list-customer.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { Subscription } from 'rxjs';
 import { Customer } from 'src/interfaces/Customer';
 import { CustomerService } from 'src/services/customer.service';
+import { CreateCustomerComponent } from '../create-customer/create-customer.component';
 
 @Component({
   selector: 'app-list-customer',
@@ -13,37 +15,37 @@ export class ListCustomerComponent implements OnInit {
 
   listado: Customer[]= {} as Customer[];
   private subscription = new Subscription();
-  counter: number;
   selectCust = {} as Customer;
+  modalRef: MdbModalRef<CreateCustomerComponent> | null = null;
 
-  constructor(private router: Router, private customerService: CustomerService ) { }
+  constructor(private router: Router, 
+    private customerService: CustomerService, 
+    private modalService: MdbModalService) { }
 
   ngOnInit(): void {
     this.getCustomer()
-    this.counter=0;
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
+  openModalCustomer() {
+    this.modalRef = this.modalService.open(CreateCustomerComponent)
+  }
+
   getCustomer(){
     this.subscription.add(
       this.customerService.getCustomers().subscribe({
         next: (respuesta: Customer[]) => {
-
-          for(const lst of respuesta){
-            this.counter+=1
-          }
-          
-              this.listado = respuesta;
-            },
-            error: () => {
-              alert('Error al traer el listado de la api');
-            },
-          })
-          );
-        }
+          this.listado = respuesta;            
+        },
+        error: () => {
+          alert('Error al traer el listado de la api');
+        },
+      })
+    );
+  }
 
   alterCustomer(cust: Customer){
     console.log(cust)
