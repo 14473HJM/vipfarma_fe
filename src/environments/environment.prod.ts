ee7aed61663ab9c6985c2cfac0cc653e9118c4b6
14473HJM <111933518+14473HJM@users.noreply.github.com>
2022-11-06 01:09:09 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   userBaseUrl: "CHANGME",
-  baseUrl: "http://farmavipbe-env.eba-9kn6pvy2.us-east-1.elasticbeanstalk.com/",
+  baseUrl: "http://farmavipbe-env.eba-9kn6pvy2.us-east-1.elasticbeanstalk.com",
 };
